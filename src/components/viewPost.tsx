550badfe8c8a73cf381839f4651446728f06f1a1
Eughami <imamosi50@gmail.com>
2020-12-26 16:32:10 +0200

    Added the more from the user template and made explore responsive insta style

--- src/components/viewPost.tsx
+++ src/components/viewPost.tsx
@@ -32,6 +32,7 @@ import {
   setCurrentUserListener,
   setCurrentUserRootDatabaseListener,
 } from '../redux/user/user.actions';
+import RelatedPosts from './relatedPosts';
 
 interface postIdInterface {
   postId: string;
@@ -225,218 +226,232 @@ const ViewPost = (props: ViewPostProps) => {
   return (
     <>
       {post && (
-        <Row justify="center" align="middle" className="full__page__post">
-          <Col
-            lg={24}
-            md={aspectRation > 1 ? 0 : 24}
-            sm={0}
-            xs={0}
-            style={{ height: '50px', width: '100%' }}
-          />
-          <Col
-            className="full__post__left__container"
-            xxl={aspectRation > 1 ? 9 : 5}
-            xl={aspectRation > 1 ? 10 : 6}
-            lg={aspectRation > 1 ? 11 : 8}
-            md={aspectRation > 1 ? 18 : 9}
-            sm={aspectRation > 1 ? 20 : 13}
-            xs={24}
-          >
-            <Col lg={0} md={aspectRation > 1 ? 24 : 0} sm={24}>
-              <div
-                className="full__post__avatar__container"
-                style={{ borderRight: '#f1e6e6 solid 1px' }}
-              >
-                <PostUser currentUser={currentUser!} post={post!} />
+        <>
+          <Row justify="center" align="middle" className="full__page__post">
+            <Col
+              lg={24}
+              md={aspectRation > 1 ? 0 : 24}
+              sm={0}
+              xs={0}
+              style={{ height: '50px', width: '100%' }}
+            />
+            <Col
+              className="full__post__left__container"
+              xxl={aspectRation > 1 ? 9 : 5}
+              xl={aspectRation > 1 ? 10 : 6}
+              lg={aspectRation > 1 ? 11 : 8}
+              md={aspectRation > 1 ? 18 : 9}
+              sm={aspectRation > 1 ? 20 : 13}
+              xs={24}
+            >
+              <Col lg={0} md={aspectRation > 1 ? 24 : 0} sm={24}>
+                <div
+                  className="full__post__avatar__container"
+                  style={{ borderRight: '#f1e6e6 solid 1px' }}
+                >
+                  <PostUser currentUser={currentUser!} post={post!} />
+                </div>
+              </Col>
+              <div>
+                <Carousel
+                  nextArrow={<RightCircleTwoTone twoToneColor="#ccc" />}
+                  prevArrow={<LeftCircleTwoTone twoToneColor="#ccc" />}
+                  arrows
+                >
+                  {post.image_url?.map((url, index) => (
+                    <div key={index}>
+                      <img
+                        style={{
+                          objectFit: 'cover',
+                          minHeight: aspectRation > 1 ? 400 : 600,
+                          height:
+                            initImageDim.height / 2 <= 400
+                              ? 400
+                              : initImageDim.height / 2,
+                          maxHeight: 600,
+                          width: '100%',
+                        }}
+                        onClick={() => history.push(`/post/${post.id}`)}
+                        alt={post.caption}
+                        src={url}
+                      />
+                    </div>
+                  ))}
+                </Carousel>
               </div>
             </Col>
-            <div>
-              <Carousel
-                nextArrow={<RightCircleTwoTone twoToneColor="#ccc" />}
-                prevArrow={<LeftCircleTwoTone twoToneColor="#ccc" />}
-                arrows
-              >
-                {post.image_url?.map((url, index) => (
-                  <div key={index}>
-                    <img
-                      style={{
-                        objectFit: 'cover',
-                        minHeight: aspectRation > 1 ? 400 : 600,
-                        height:
-                          initImageDim.height / 2 <= 400
-                            ? 400
-                            : initImageDim.height / 2,
-                        maxHeight: 600,
-                        width: '100%',
-                      }}
-                      onClick={() => history.push(`/post/${post.id}`)}
-                      alt={post.caption}
-                      src={url}
+            <Col
+              className="full__post__right__container"
+              xxl={aspectRation > 1 ? 7 : 5}
+              xl={aspectRation > 1 ? 8 : 6}
+              lg={aspectRation > 1 ? 9 : 8}
+              md={aspectRation > 1 ? 18 : 9}
+              sm={aspectRation > 1 ? 20 : 13}
+              xs={24}
+            >
+              {/* hide on small size screen */}
+              <Col lg={24} md={aspectRation > 1 ? 0 : 24} sm={0} xs={0}>
+                <div className="full__post__avatar__container">
+                  <PostUser currentUser={currentUser!} post={post!} />
+                </div>
+                <div
+                  className="full__post__comments__container"
+                  style={{
+                    minHeight: aspectRation > 1 ? 160 : 360,
+                    height:
+                      initImageDim.height / 2 <= 400
+                        ? 160
+                        : initImageDim.height / 2 - 240,
+                    maxHeight: 360,
+                  }}
+                >
+                  <PerfectScrollbar>
+                    <PostComments full={true} post={post!} />
+                  </PerfectScrollbar>
+                </div>
+                <div className="full__page__post__actions__container">
+                  <Row justify="start" align="top">
+                    <Col span={14}>
+                      <PostActions currentUser={currentUser!} post={post} />
+                    </Col>
+                    <Col span={10}>
+                      <PostEventTime post={post} />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={4}>
+                      <PostLikes post={post} />
+                    </Col>
+                    <Col span={20}>
+                      <PostTags post={post} />
+                    </Col>
+                    <Row>
+                      <PostCaption post={post} />
+                    </Row>
+                  </Row>
+                </div>
+                <Row className="full__post__add__comment__container">
+                  <Col flex="auto">
+                    <AsyncMention
+                      border="none"
+                      value={comment.comment}
+                      onChange={handleCommentChange}
+                      placeholder="Add a comment..."
                     />
-                  </div>
-                ))}
-              </Carousel>
-            </div>
-          </Col>
-          <Col
-            className="full__post__right__container"
-            xxl={aspectRation > 1 ? 7 : 5}
-            xl={aspectRation > 1 ? 8 : 6}
-            lg={aspectRation > 1 ? 9 : 8}
-            md={aspectRation > 1 ? 18 : 9}
-            sm={aspectRation > 1 ? 20 : 13}
-            xs={24}
-          >
-            {/* hide on small size screen */}
-            <Col lg={24} md={aspectRation > 1 ? 0 : 24} sm={0} xs={0}>
-              <div className="full__post__avatar__container">
-                <PostUser currentUser={currentUser!} post={post!} />
-              </div>
-              <div
-                className="full__post__comments__container"
-                style={{
-                  minHeight: aspectRation > 1 ? 160 : 360,
-                  height:
-                    initImageDim.height / 2 <= 400
-                      ? 160
-                      : initImageDim.height / 2 - 240,
-                  maxHeight: 360,
-                }}
-              >
-                <PerfectScrollbar>
-                  <PostComments full={true} post={post!} />
-                </PerfectScrollbar>
-              </div>
-              <div className="full__page__post__actions__container">
-                <Row justify="start" align="top">
-                  <Col span={14}>
-                    <PostActions currentUser={currentUser!} post={post} />
                   </Col>
-                  <Col span={10}>
-                    <PostEventTime post={post} />
+                  <Col flex="50px">
+                    <Button
+                      loading={postCommentLoading}
+                      onClick={() =>
+                        onPostComment(
+                          setPostCommentLoading,
+                          currentUserInfo!,
+                          post.id,
+                          post.user.username,
+                          comment,
+                          currentUserToken!
+                        ).finally(() => resetCommentForm())
+                      }
+                      disabled={comment.comment.length === 0}
+                      style={{
+                        height: '100%',
+                        // background: 'transparent',
+                        border: 'none',
+                      }}
+                    >
+                      Post
+                    </Button>
                   </Col>
                 </Row>
-                <Row>
-                  <Col span={4}>
-                    <PostLikes post={post} />
-                  </Col>
-                  <Col span={20}>
-                    <PostTags post={post} />
-                  </Col>
-                  <Row>
-                    <PostCaption post={post} />
+              </Col>
+              {/* show on small size screen */}
+              <Col lg={0} md={aspectRation > 1 ? 24 : 0} sm={24}>
+                <div className="full__page__post__actions__container">
+                  <Row justify="start" align="top">
+                    <Col span={12}>
+                      <PostActions currentUser={currentUser!} post={post} />
+                    </Col>
+                    <Col span={12}>
+                      <PostEventTime post={post} />
+                    </Col>
                   </Row>
-                </Row>
-              </div>
-              <Row className="full__post__add__comment__container">
-                <Col flex="auto">
-                  <AsyncMention
-                    border="none"
-                    value={comment.comment}
-                    onChange={handleCommentChange}
-                    placeholder="Add a comment..."
-                  />
-                </Col>
-                <Col flex="50px">
-                  <Button
-                    loading={postCommentLoading}
-                    onClick={() =>
-                      onPostComment(
-                        setPostCommentLoading,
-                        currentUserInfo!,
-                        post.id,
-                        post.user.username,
-                        comment,
-                        currentUserToken!
-                      ).finally(() => resetCommentForm())
-                    }
-                    disabled={comment.comment.length === 0}
-                    style={{
-                      height: '100%',
-                      // background: 'transparent',
-                      border: 'none',
-                    }}
-                  >
-                    Post
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-            {/* show on small size screen */}
-            <Col lg={0} md={aspectRation > 1 ? 24 : 0} sm={24}>
-              <div className="full__page__post__actions__container">
-                <Row justify="start" align="top">
-                  <Col span={12}>
-                    <PostActions currentUser={currentUser!} post={post} />
-                  </Col>
-                  <Col span={12}>
-                    <PostEventTime post={post} />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col span={4}>
-                    <PostLikes post={post} />
-                  </Col>
-                  <Col span={20}>
-                    <PostTags post={post} />
-                  </Col>
                   <Row>
-                    <PostCaption post={post} />
+                    <Col span={4}>
+                      <PostLikes post={post} />
+                    </Col>
+                    <Col span={20}>
+                      <PostTags post={post} />
+                    </Col>
+                    <Row>
+                      <PostCaption post={post} />
+                    </Row>
                   </Row>
+                </div>
+                <Row className="full__post__add__comment__container">
+                  <Col flex="auto">
+                    <AsyncMention
+                      value={comment.comment}
+                      onChange={handleCommentChange}
+                      placeholder="Add a comment..."
+                    />
+                  </Col>
+                  <Col flex="50px">
+                    <Button
+                      loading={postCommentLoading}
+                      onClick={() =>
+                        onPostComment(
+                          setPostCommentLoading,
+                          currentUserInfo!,
+                          post.id,
+                          post.user.username,
+                          comment,
+                          currentUserToken!
+                        ).finally(() => resetCommentForm())
+                      }
+                      disabled={comment.comment.length === 0}
+                      style={{
+                        height: '100%',
+                        // background: 'transparent',
+                        border: 'none',
+                      }}
+                    >
+                      Post
+                    </Button>
+                  </Col>
                 </Row>
-              </div>
-              <Row className="full__post__add__comment__container">
-                <Col flex="auto">
-                  <AsyncMention
-                    value={comment.comment}
-                    onChange={handleCommentChange}
-                    placeholder="Add a comment..."
-                  />
-                </Col>
-                <Col flex="50px">
-                  <Button
-                    loading={postCommentLoading}
-                    onClick={() =>
-                      onPostComment(
-                        setPostCommentLoading,
-                        currentUserInfo!,
-                        post.id,
-                        post.user.username,
-                        comment,
-                        currentUserToken!
-                      ).finally(() => resetCommentForm())
-                    }
-                    disabled={comment.comment.length === 0}
-                    style={{
-                      height: '100%',
-                      // background: 'transparent',
-                      border: 'none',
-                    }}
-                  >
-                    Post
-                  </Button>
-                </Col>
-              </Row>
-              <div
-                className="full__post__comments__container"
-                style={{
-                  minHeight: aspectRation > 1 ? 160 : 360,
-                  height:
-                    initImageDim.height / 2 <= 400
-                      ? 160
-                      : initImageDim.height / 2 - 240,
-                  maxHeight: 360,
-                }}
-              >
-                <PerfectScrollbar>
-                  <PostComments full={true} post={post!} />
-                </PerfectScrollbar>
-              </div>
-            </Col>
+                <div
+                  className="full__post__comments__container"
+                  style={{
+                    minHeight: aspectRation > 1 ? 160 : 360,
+                    height:
+                      initImageDim.height / 2 <= 400
+                        ? 160
+                        : initImageDim.height / 2 - 240,
+                    maxHeight: 360,
+                  }}
+                >
+                  <PerfectScrollbar>
+                    <PostComments full={true} post={post!} />
+                  </PerfectScrollbar>
+                </div>
+              </Col>
 
-            <Col lg={24} md={aspectRation > 1 ? 0 : 24} sm={0} xs={0}></Col>
-          </Col>
-        </Row>
+              <Col lg={24} md={aspectRation > 1 ? 0 : 24} sm={0} xs={0}></Col>
+            </Col>
+          </Row>
+          <Row justify="center">
+            <Col
+              xl={18}
+              lg={20}
+              md={24}
+              sm={0}
+              xs={0}
+              style={{ height: 400, marginTop: 100, marginBottom: 50 }}
+            >
+              <RelatedPosts />
+            </Col>
+          </Row>
+        </>
       )}
     </>
   );
